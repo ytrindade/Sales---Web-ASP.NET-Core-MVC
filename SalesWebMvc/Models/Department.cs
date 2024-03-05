2468cf36f8e059afdc2a4695dfacff215ed5aff1
ytrindade <yedi.trindade@gmail.com>
2024-03-05 15:13:22 -0300

    First MVC - Department

---
+++ SalesWebMvc/Models/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace SalesWebMvc.Models
+{
+    public class Department
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+       
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return 0;
+        }
+
+    }
+}
