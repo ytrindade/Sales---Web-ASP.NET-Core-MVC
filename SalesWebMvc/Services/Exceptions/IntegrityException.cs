67027ac343e17fca157c8aa9503a23f9a27fd63e
ytrindade <yedi.trindade@gmail.com>
2024-03-13 15:24:52 -0300

    Exception handling for delete (referential integrity)

---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,12 @@
+﻿namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+
+        public IntegrityException(string message) : base(message)
+        {
+
+        }
+
+    }
+}
